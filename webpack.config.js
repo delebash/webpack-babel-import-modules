1332c780113e43edfe8b4a21565867e1ff142a36
Daniel Elebash <danelebash@hotmail.com>
2016-04-16 12:50:08 -0400

    added npm and sass webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var path = require('path');
 const ProvidePlugin = require('webpack/lib/ProvidePlugin');
 const webpack = require("webpack");
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
 
 module.exports = {
@@ -23,7 +24,7 @@ module.exports = {
             allChunks: true
         })
     ],
-  devtool: 'inline-source-map',
+    devtool: 'source-map',
     devServer: {
         host: 'localhost',
         port: 3000
@@ -45,7 +46,6 @@ module.exports = {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         },
-        //{ test: /\.css?$/, loader: 'style!css'},
         { test: /\.html$/, loader: 'html' },
         { test: /\.(png|gif|jpg)$/, loader: 'url', query: { limit: 8192 } },
         { test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url', query: { limit: 10000, mimetype: 'application/font-woff2' } },
@@ -53,5 +53,13 @@ module.exports = {
         { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file' },
        // { test: /\.scss$/, loader: 'style!css?sourceMap!postcss!sass?sourceMap'}
     ]
-  }
+  },
+    postcss: [
+        autoprefixer({
+            browsers: ['last 2 versions']
+        })
+    ],
+    sassLoader: {
+        includePaths: [path.resolve(__dirname, "./sass")]
+    }
 };
